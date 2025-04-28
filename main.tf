a14e5581fb87985d5f29c975924371e865876950
chaitanyak <navchaitanya@gmail.com>
2025-04-28 08:56:11 +0530

    Updated IAC code

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "azurerm_network_interface" "this" {
   name                = var.nic_name
   location            = var.location
   resource_group_name = var.resource_group_name
-
   ip_configuration {
     name                          = "${var.nic_name}-ipconfig"
     subnet_id                     = var.subnet_id
