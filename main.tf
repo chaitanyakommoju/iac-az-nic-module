b99946d9969b53e80feec7a208d6c294a504b4cd
chaitanyak <navchaitanya@gmail.com>
2025-04-29 00:39:10 +0530

    Updated IAC code

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_public_ip" "this" {
   name                = "${var.nic_name}-pip"
   resource_group_name = var.resource_group_name
   location            = var.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Basic"
 
   tags = var.tags
